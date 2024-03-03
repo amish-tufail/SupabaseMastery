4179a93dddb67f595db18a59354151bae374e9cd
Amish <amish.tufail2002@gmail.com>
2024-03-03 21:51:59 +0500

    - Added SupabaseClient - Project Structure Setup

---
+++ SupabaseMastery/Client/Client.swift
@@ -0,0 +1,14 @@
+//
+//  Client.swift
+//  SupabaseMastery
+//
+//  Created by Amish Tufail on 03/03/2024.
+//
+
+import Foundation
+import Supabase
+
+class Client {
+    // SupabaseClient is the entry point into Supabase
+    let client = SupabaseClient(supabaseURL: URL(string: "https://ckwlquluslxpelcwssch.supabase.co")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrd2xxdWx1c2x4cGVsY3dzc2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0ODM4NzUsImV4cCI6MjAyNTA1OTg3NX0.ZKkcJmodqEp8-dVSkJw5cFif32CunVnZ-NWfwLlqQnU")
+}
