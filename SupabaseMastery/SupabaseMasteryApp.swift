f67929e84012e96f187e7ab20c8d4127d0750c1f
Amish <amish.tufail2002@gmail.com>
2024-03-03 21:36:49 +0500

    Initial Commit

---
+++ SupabaseMastery/SupabaseMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseMasteryApp.swift
+//  SupabaseMastery
+//
+//  Created by Amish Tufail on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
